0d8eaeee0e86d638b27b1d7e1a0f32704adff5eb
lewiskimaru <139235175+lewiskimaru@users.noreply.github.com>
2024-01-01 13:05:47 +0300

    Update pingpong.js

--- pingpong.js
+++ pingpong.js
@@ -2,8 +2,8 @@
 const { body } = document;
 const canvas = document.createElement('canvas');
 const context = canvas.getContext('2d');
-const width = 500;
-const height = 700;
+const width = 400;
+const height = 600;
 const screenWidth = window.screen.width;
 const canvasPosition = screenWidth / 2 - width / 2;
 const isMobile = window.matchMedia('(max-width: 600px)');
@@ -65,8 +65,8 @@ function renderCanvas() {
   // Dashed Center Line
   context.beginPath();
   context.setLineDash([4]);
-  context.moveTo(0, 350);
-  context.lineTo(500, 350);
+  context.moveTo(0, 300);
+  context.lineTo(500, 300);
   context.strokeStyle = 'grey';
   context.stroke();
 
